49920cca578a98faa2412c9bfd54d146d22b2971
Thien Trieu <thien.trieu@telus.com>
2022-11-24 13:05:06 -0800

    breedFetcher.js and mocha/chai test case created

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,21 @@
 const request = require('request');
-const breedName = process.argv.slice(2);
 
-
-const fetchBreedDescription = function(breedName, callback) {
-
+const fetchBreedDescription = (breedName, callback) => {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     if (error) {
-          console.log(`ERR accessing API`);
-          console.log(`> ${error}`);
+      callback(error, null);
       return;
     }
+
     const data = JSON.parse(body);
+
     try {
-      console.log(data[0].description);
+      callback(null, data[0].description);
     } catch (error) {
-      console.log(`Breed was not found`);
+      error = "Invalid/non-existent breed.";
+      callback(error, null);
     }
   });
-
 };
 
-fetchBreedDescription(breedName)
+module.exports = { fetchBreedDescription };
