37d7e605b60bbc1b6f39b5d7504537e143c880ba
Thien Trieu <thien.trieu@telus.com>
2022-11-24 09:35:23 -0800

    commiting changes to breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,23 @@
 const request = require('request');
-const args = process.argv.slice(2);
+const breedName = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (error, response, body) => {
-  const data = JSON.parse(body);
-  try {
-    console.log(data[0].description);
-  } catch (error) {
-    console.log("There was an error", error.name);
-  }
-});
+
+const fetchBreedDescription = function(breedName, callback) {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+          console.log(`ERR accessing API`);
+          console.log(`> ${error}`);
+      return;
+    }
+    const data = JSON.parse(body);
+    try {
+      console.log(data[0].description);
+    } catch (error) {
+      console.log(`Breed was not found`);
+    }
+  });
+
+};
+
+fetchBreedDescription(breedName)
